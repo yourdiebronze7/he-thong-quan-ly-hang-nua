b7c0f46ab25af0d3e33c88a64d041f829f8df47f
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-10-04 17:02:00 +0000

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hệ Thống Quản Lý Hàng Hoá</h1>
+      <p>Chào mừng bạn đến với hệ thống của chúng tôi.</p>
+    </div>
+  );
+}
+
+export default App;
